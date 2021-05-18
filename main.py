5bb7ca79c4878e7d36656f0146a7f93b2d52fbca
yagamiraku <yagami.rakuraku@gmail.com>
2021-05-18 11:43:51 +0800

    upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import json
+import os
+import traceback
+from time import sleep
+
+import requests
+
+chia_command = 'C:\\Users\\31535\\AppData\\Local\\chia-blockchain\\app-1.1.5\\resources\\app.asar.unpacked\\daemon\\chia.exe show -a'
+
+
+def get_nodes():
+    try:
+        res = requests.get('https://chia.powerlayout.com/nodes?block_height=false')
+        if res.status_code == 200:
+            return json.loads(res.content.decode('UTF-8'))
+    except:
+        traceback.print_exc()
+
+
+if __name__ == '__main__':
+    nodes = get_nodes()
+    while True:
+        for node in nodes['nodes']:
+            ret = os.system(chia_command + node)
+        sleep(60 * 5)
